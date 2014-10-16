ba2b4723e76d9314a6048270c5d9cb3e09168cf4
Justin Gray <justin.s.gray@gmail.com>
2014-10-16 14:11:52 -0400

    bandb nonlinear now sizes its arrays correctly

--- nonlintest.py
+++ nonlintest.py
@@ -4,14 +4,14 @@ from openmdao.main.api import Assembly
 from openmdao.lib.drivers.api import IterateUntil
 from openmdao.lib.casehandlers.api import JSONCaseRecorder
 
-from branch_and_bound.branch_and_bound import BranchBoundNonLinear
+from branch_and_bound.branch_and_bound_nonlinear import BranchBoundNonLinear
 from branch_and_bound.nonlinear import NonLinearTestProblem, BandBSLSQPdriver
 
 class NonLinTest(Assembly):
 
     def configure(self):
         self.add('driver', IterateUntil())
-        self.add('branchbound_algorithm', BranchBoundNonLinear(n_int = 2, n_cont = 0))
+        self.add('branchbound_algorithm', BranchBoundNonLinear(n_int = 2, n_contin = 0))
         self.add('nonlinopt', BandBSLSQPdriver())
         self.add('nonlin_test_prob', NonLinearTestProblem())
 
@@ -27,16 +27,16 @@ class NonLinTest(Assembly):
         self.nonlinopt.workflow.add('nonlin_test_prob')
 
         #data connections
+        # Connect solver component with the Branch  and Bound Algorithm Component (return results)
+        self.connect('nonlin_test_prob.x',     'branchbound_algorithm.xopt_current')
+        self.connect('nonlin_test_prob.f',     'branchbound_algorithm.relaxed_obj_current')
+        self.connect('nonlinopt.exit_flag',    'branchbound_algorithm.exitflag_NLP')
+
 
         # Connect Airline Allocation SubProblem Component with Branch  and Bound Algorithm Component and the solver
         # Connect Branch  and Bound Algorithm Component with the solver component
         self.connect('branchbound_algorithm.lb', 'nonlinopt.lb')
         self.connect('branchbound_algorithm.ub', 'nonlinopt.ub')
-
-        # Connect solver component with the Branch  and Bound Algorithm Component (return results)
-        self.connect('nonlin_test_prob.x',     'branchbound_algorithm.xopt_current')
-        self.connect('nonlin_test_prob.f',     'branchbound_algorithm.relaxed_obj_current')
-        self.connect('nonlinopt.exit_flag',    'branchbound_algorithm.exitflag_NLP')
 
         self.driver.add_stop_condition('branchbound_algorithm.exec_loop != 0')
         self.driver.max_iterations = 1000000
